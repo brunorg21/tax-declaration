a779f2a7bc33cb8775de78c465d642e9ea6c0b7b
brunorg21 <rbrunog21@gmail.com>
2025-01-17 13:26:35 -0300

    refactor: change param type

--- server/src/database/repositories/user-repository.ts
+++ server/src/database/repositories/user-repository.ts
@@ -2,5 +2,5 @@ import { Prisma, User } from '@prisma/client';
 
 export abstract class UserRepository {
   abstract findByEmail(email: string): Promise<User | null>;
-  abstract save(user: Prisma.UserCreateInput): Promise<void>;
+  abstract save(user: Prisma.UserUncheckedCreateInput): Promise<void>;
 }
