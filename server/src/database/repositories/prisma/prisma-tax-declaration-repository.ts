01bb34039f915d8bacbabee21f6efd07f9c123e4
brunorg21 <rbrunog21@gmail.com>
2025-01-17 13:27:20 -0300

    feat: add find by id method on repository

--- server/src/database/repositories/prisma/prisma-tax-declaration-repository.ts
+++ server/src/database/repositories/prisma/prisma-tax-declaration-repository.ts
@@ -1,16 +1,64 @@
-import { Prisma, TaxDeclaration } from '@prisma/client';
+import { TaxDeclaration } from '@prisma/client';
 
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma-service';
 import { TaxDeclarationRepository } from '../tax-declaration-repository';
+import {
+  CreateTaxDeclarationRequest,
+  UpdateTaxDeclarationRequest,
+} from '@/tax-declaration/tax-declaration.service';
 
 @Injectable()
 export class PrismaTaxDeclarationRepository
   implements TaxDeclarationRepository
 {
   constructor(private prismaService: PrismaService) {}
-  async save(taxDeclaration: Prisma.TaxDeclarationCreateInput): Promise<void> {
-    await this.prismaService.taxDeclaration.create({ data: taxDeclaration });
+  async findById(taxDeclarationId: string): Promise<TaxDeclaration | null> {
+    const taxDeclaration = await this.prismaService.taxDeclaration.findUnique({
+      where: {
+        id: taxDeclarationId,
+      },
+    });
+
+    if (!taxDeclaration) {
+      return null;
+    }
+
+    return taxDeclaration;
+  }
+  async save({
+    alimony,
+    complementarySocialSecurityContribution,
+    dependents,
+    earnings,
+    educationExpenses,
+    medicalExpenses,
+    socialSecurityContribution,
+    status,
+    userId,
+  }: CreateTaxDeclarationRequest): Promise<void> {
+    await this.prismaService.taxDeclaration.create({
+      data: {
+        complementarySocialSecurityContribution,
+        alimony,
+        earnings,
+        educationExpenses,
+        medicalExpenses,
+        socialSecurityContribution,
+        status,
+        dependents: {
+          createMany: {
+            data: dependents.map((e) => ({
+              birthDate: e.birthDate,
+              cpf: e.cpf,
+              email: e.email,
+              name: e.name,
+            })),
+          },
+        },
+        userId,
+      },
+    });
   }
   async findManyByYear(
     userId: string,
@@ -46,14 +94,35 @@ export class PrismaTaxDeclarationRepository
     return taxDeclaration;
   }
   async update(
-    data: Prisma.TaxDeclarationUpdateInput,
+    data: UpdateTaxDeclarationRequest,
     taxDeclarationId: string,
   ): Promise<void> {
+    const updateData: any = {
+      ...data,
+      dependents: undefined,
+    };
+
+    if (data.dependents && data.dependents.length > 0) {
+      updateData.dependents = {
+        updateMany: {
+          data: data.dependents.map((e) => ({
+            birthDate: e.birthDate,
+            cpf: e.cpf,
+            email: e.email,
+            name: e.name,
+          })),
+          where: {
+            taxDeclarationId,
+          },
+        },
+      };
+    }
+
     await this.prismaService.taxDeclaration.update({
       where: {
         id: taxDeclarationId,
       },
-      data,
+      data: updateData,
     });
   }
 }
