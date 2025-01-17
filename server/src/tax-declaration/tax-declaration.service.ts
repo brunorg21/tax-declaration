e7f171df565699594ff02d6e53ca5287a4c5a12d
brunorg21 <rbrunog21@gmail.com>
2025-01-17 13:28:37 -0300

    feat: add findUnique on service

--- server/src/tax-declaration/tax-declaration.service.ts
+++ server/src/tax-declaration/tax-declaration.service.ts
@@ -1,12 +1,44 @@
 import { TaxDeclarationRepository } from '@/database/repositories/tax-declaration-repository';
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+
+export interface CreateTaxDeclarationRequest {
+  medicalExpenses: number;
+  educationExpenses: number;
+  earnings: number;
+  alimony: number;
+  socialSecurityContribution: number;
+  complementarySocialSecurityContribution: number;
+  status: 'UNSUBMMITED' | 'SUBMMITED';
+  dependents: {
+    name: string;
+    birthDate: Date;
+    cpf: string;
+    email: string;
+  }[];
+  userId: string;
+}
+
+export interface UpdateTaxDeclarationRequest {
+  medicalExpenses: number;
+  educationExpenses: number;
+  earnings: number;
+  alimony: number;
+  socialSecurityContribution: number;
+  complementarySocialSecurityContribution: number;
+  status: 'UNSUBMMITED' | 'SUBMMITED';
+  dependents: {
+    name: string;
+    birthDate: Date;
+    cpf: string;
+    email: string;
+  }[];
+}
 
 @Injectable()
 export class TaxDeclarationService {
   constructor(private taxDeclarationRepository: TaxDeclarationRepository) {}
 
-  async create(taxDeclaration: Prisma.TaxDeclarationCreateInput) {
+  async create(taxDeclaration: CreateTaxDeclarationRequest) {
     await this.taxDeclarationRepository.save(taxDeclaration);
   }
 
@@ -14,10 +46,11 @@ export class TaxDeclarationService {
     return await this.taxDeclarationRepository.findManyByYear(userId, year);
   }
 
-  async update(
-    taxDeclarationId: string,
-    data: Prisma.TaxDeclarationUpdateInput,
-  ) {
+  async update(taxDeclarationId: string, data: UpdateTaxDeclarationRequest) {
     await this.taxDeclarationRepository.update(data, taxDeclarationId);
+  }
+
+  async findUnique(taxDeclarationId: string) {
+    return await this.taxDeclarationRepository.findUnique(taxDeclarationId);
   }
 }
