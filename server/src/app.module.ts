a136de79efea913fab0c6670f52aba3226b69d20
brunorg21 <rbrunog21@gmail.com>
2025-01-17 10:23:57 -0300

    feat: add TaxDeclarationModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database-module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TaxDeclarationModule } from './tax-declaration/tax-declaration.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UserModule],
+  imports: [AuthModule, UserModule, TaxDeclarationModule],
   controllers: [AppController],
   providers: [AppService],
 })
